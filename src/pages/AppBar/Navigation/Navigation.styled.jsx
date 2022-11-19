23a493440ea2b32995d26579e2ccdf824368202d
Vialii Hordiichuk <vgordiychuk5@gmail.com>
2022-11-19 02:27:27 +0200

    hw-8

--- src/pages/AppBar/Navigation/Navigation.styled.jsx
+++ src/pages/AppBar/Navigation/Navigation.styled.jsx
@@ -2,6 +2,9 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
 export const NavItem = styled(NavLink)`
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
   text-decoration: none;
   margin-left: 50px;
   &.active {
